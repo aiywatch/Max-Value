d37c40de1abe432e3402a0667c2cd2c50d4aa9fe
aiywatch <aiycp35@gmail.com>
2016-06-21 18:51:21 +0000

    renter

---
+++ renter.rb
@@ -0,0 +1,23 @@
+# must be baller and either furnished or rent cheaper than 2100
+def rent?(furnished, rent, baller)
+  
+  baller && (furnished || rent < 2100)
+  # if baller && (furnished || rent < 2100)
+  #   return true
+  # else
+  #   return false
+  # end
+end
+
+###
+# Add your "test" ("driver") code below in order to "test drive" (run) your method above...
+# The test code will call the method with different permutations of options and output the result each time.
+# This way, you will be able to run the renter.rb file from the CLI and look at the output of your "tests" to validate if the method works.
+# Without the test code, it will be hard for you to know if this method is working as it should or not.
+###
+puts rent?(true, 4000, false)
+puts rent?(true, 1500, false)
+puts rent?(true, 4000, true)
+puts rent?(true, 1500, true)
+puts rent?(false, 4000, true)
+puts rent?(false, 1500, true)
